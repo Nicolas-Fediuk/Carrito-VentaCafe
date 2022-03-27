e0f08d419c6cbf7c0d403e27352d02ee8b17eea8
unknown <nicolasfediuk@gmail.com>
2022-03-27 13:18:18 -0300

    listo favorito

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 //nav
-const botonNavBar = document.querySelector("#nav-corazon");
+const botonNavBar = document.querySelector("#nav-bar");
 const navBar = document.querySelector(".nav-action");
+const buscadorHtml = document.querySelector(".buscadorHtml");
 
-//carrito y ventana
+//carrito , ventana, favoritos y buscador
 const carrito = document.querySelector(".carrito");
 const btnCarrito = document.querySelector("#carrito");
+const buscador = document.querySelector("#buscador");
+const Afavorito = document.querySelector("#Afavorito");
+const favorito = document.querySelector(".favorito");
+const btnFavorito = document.querySelector("#btnFavorito");
+const buscadorInput = document.querySelector("#buscadorInput");
 
 //carrito Tazas
 const contenedorCarrito = document.querySelector(".carrito table tbody");
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
 const listarTaza = document.querySelector("#agrega-taza-carrito");
 const borrarTaza = document.querySelector("#vaciar-carrito");
+const contenedorFavorito = document.querySelector(".favorito table tbody");
 
 //carrito cafe
 const listarCafe = document.querySelector("#agregar-cafe-carrito");
 
 let articuloCarrito = [];
+let articuloFavorito = [];
 
 cargarEventListeners();
 
@@ -25,6 +33,9 @@ function cargarEventListeners(){
     //navBar
     botonNavBar.addEventListener("click",mostrarNav);
 
+    //busscador
+    buscador.addEventListener("click",mostrarBuscador);
+
     //carrito
     btnCarrito.addEventListener("click",mostrarCarrito);
 
@@ -32,10 +43,24 @@ function cargarEventListeners(){
     listarTaza.addEventListener("click",agregarTaza);
 
     //carrito Cafe
-    listarCafe.addEventListener("click", agregarCafe )
+    listarCafe.addEventListener("click", agregarCafe);
+
+    btnFavorito.addEventListener("click", agregarFavorito);
+
+    Afavorito.addEventListener("click",mostrarFavorito);
 
     //eliminar del carrito
     contenedorCarrito.addEventListener("click", eliminarProducto);
+
+    //buscador
+    buscadorInput.addEventListener("input",mostrarBuscadorHtml);
+
+    //muestra los productos del localStorage
+    document.addEventListener("DOMContentLoaded", ()=>{
+        articuloFavorito = JSON.parse(localStorage.getItem("favorito")) || [];
+
+        favoritoCafeHtml();
+    });
 
     //vaciar el carrito
     borrarTaza.addEventListener("click", (e) =>{
@@ -44,6 +69,17 @@ function cargarEventListeners(){
         limpiarHtml();
     });
 
+}
+
+function mostrarBuscador(){
+    if(buscadorHtml.style.display == "none"){
+        buscadorHtml.style.display = 'block';
+        carrito.style.display = "none";
+        favorito.style.display = "none";
+    }
+    else{
+        buscadorHtml.style.display = 'none';
+    }
 }
 
 function mostrarNav(){
@@ -58,6 +94,8 @@ function mostrarNav(){
 function mostrarCarrito(){
     if(carrito.style.display=="none"){
         carrito.style.display = "block";
+        buscadorHtml.style.display = 'none';
+        favorito.style.display = "none";
     }
     else{
         carrito.style.display = "none";
@@ -235,6 +273,110 @@ function limpiarHtml(){
         contenedorCarrito.removeChild(contenedorCarrito.firstChild);
     }
 }
+function limpiarHtmlFavorito(){
+    while(contenedorFavorito.firstChild){
+        contenedorFavorito.removeChild(contenedorFavorito.firstChild);
+    }
+}
+
+function mostrarFavorito(){
+    if(favorito.style.display == "none"){
+        favorito.style.display = "block";
+        carrito.style.display = "none";
+        buscadorHtml.style.display = 'none';
+    }
+    else{
+        favorito.style.display = "none";
+    }
+}
+
+function agregarFavorito(e){
+
+    e.preventDefault();
+
+    if(e.target.classList.contains("AfavoritoProducto")){
+
+        const cafeSeleccionado=e.target.parentElement.parentElement;
+
+        leerDatosCafeFavorito(cafeSeleccionado);
+    }
+}
+
+function leerDatosCafeFavorito(cafe){
+
+    const infoCafe = {
+        imagen: cafe.querySelector("img").src,
+        titulo: cafe.querySelector("h4").textContent,
+        precio: cafe.querySelector(".producto-precio p:first-of-type").textContent,
+        id: cafe.querySelector("a").getAttribute("data-id"),
+    };
+
+    const existe = articuloFavorito.some(cafe => cafe.id === infoCafe.id);
+
+    if(existe === false){
+        articuloFavorito = [...articuloFavorito,infoCafe];
+        
+    }
+    else{
+        return true;
+    }
+
+    favoritoCafeHtml();
+}
+
+function favoritoCafeHtml(){
+
+    limpiarHtmlFavorito();
+
+
+    articuloFavorito.forEach(cafe =>{
+        const{imagen,titulo,precio,id} = cafe;
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>
+                <img src="${imagen}">
+            </td>
+            <td>
+                ${titulo}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                <a href="#" class="borrarFavorito" data-id="${id}">X</a>
+            </td>
+        `;
+        contenedorFavorito.appendChild(row);
+
+        if(articuloFavorito.length > 0){
+            const btnBorrarFavorito = document.querySelector(".borrarFavorito");
+            articuloFavorito.forEach(cafe =>{
+                btnBorrarFavorito.onclick = () =>{
+                    borrarCafe(cafe.id);
+                };
+            });
+        }
+        
+    });
+
+    sincronizarStorage();
+}
+
+function sincronizarStorage(){
+    localStorage.setItem("favorito",JSON.stringify(articuloFavorito));
+}
+
+function borrarCafe(id){
+    articuloFavorito = articuloFavorito.filter(cafe => cafe.id !== id);
+    favoritoCafeHtml();
+}
+
+function mostrarBuscadorHtml(e){
+    if (e.target.value === mainBuscador){
+        console.log("si");
+    }
+}
+
 
 
 
